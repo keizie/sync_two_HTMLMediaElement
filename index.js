24d8f2e3ae4e514a309939c18da1f48c4f2b90c4
Jeong-Hee Kang <jeonghee.kang@ejn.gg>
2022-09-01 15:18:13 +0900

    fix(ytdl): set quality filter similar to audioandvideo formats

--- index.js
+++ index.js
@@ -19,20 +19,33 @@ app.get('/yt2', async (req, res) => {
   try {
     const info = await ytdl.getInfo(id, { quality: ['43', '18', '22'] })
     console.log(`[${new Date()}] [2/3] got info: ${id}`)
+    const formats = ytdl.filterFormats(info.formats, 'audioandvideo').sort((a, b) => {
+		  if (a.container === 'webm' && b.container !== 'webm') return -1;
+			else if (a.container !== 'webm' && b.container === 'webm') return 1;
+			return a.audioBitrate - b.audioBitrate;
+    });
+    const audioBitrates = formats.map(a => a.audioBitrate);
+    const widths = formats.map(a => a.width);
+    const heights = formats.map(a => a.height);
+    const fpss = formats.map(a => a.fps);
+    // console.log(audioBitrates, widths, heights);
+
     const formatsA = ytdl.filterFormats(info.formats, 'audioonly').sort((a, b) => {
 		  if (a.container === 'webm' && b.container !== 'webm') return -1;
 			else if (a.container !== 'webm' && b.container === 'webm') return 1;
 			return a.audioBitrate - b.audioBitrate;
-    })
+    }).filter((a) => audioBitrates.map(b => a.audioBitrate >= b.audioBitrate).length > 0);
     const formatsV = ytdl.filterFormats(info.formats, 'videoonly').sort((a, b) => {
 		  if (a.container === 'webm' && b.container !== 'webm') return -1;
 			else if (a.container !== 'webm' && b.container === 'webm') return 1;
-			return a.audioBitrate - b.audioBitrate;
-    })
+        console.log(a.bitrate);
+			return a.bitrate - b.bitrate;
+    }).filter((a) => widths.includes(a.width) && heights.includes(a.height) && fpss.includes(a.fps));
     console.log(`[${new Date()}] [3/3] choose formats: ${id}`)
+
     const data = {
-        audio: formatsA.at(1)?.url ?? '',
-        video: formatsV.at(1)?.url ?? '',
+        audio: formatsA.at(0)?.url ?? '',
+        video: formatsV.at(0)?.url ?? '',
     };
     // console.log(data);
     cache.put(cacheKey, data, 20 * 1e3)
